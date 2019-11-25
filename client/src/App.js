153fa1c4f79d77e2aa160d2872df69cea9e9d990
Yuri Lee <leyuri97@gmail.com>
2019-11-26 00:47:29 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { declaredPredicate } from '@babel/types';
 
 const styles = theme => ({
 
@@ -18,17 +20,35 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
+
+/*
+1) constructor()
+2) componentWillMount()
+3) render()
+4) componentDidMount()
+*/
+
+/*
+
+props or state => shouldComponentUpdate()
+
+*/
 
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -38,6 +58,11 @@ class App extends Component {
     const response = await fetch('api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render(){
@@ -62,21 +87,16 @@ class App extends Component {
                     <Customer
                       key={c.id}
                       //map을 이용할 때는 key를 사용해야 한다
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}
-                    />
-                    );
-                  }) : ""}
+                      id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+                  }) : 
+
+                  <TableRow>
+                    <TableCell colSpan="6" align="center">
+                      <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                    </TableCell>
+                  </TableRow>}
             </TableBody>
         </Table>
-
-
-
-
       </Paper>
     );
   }
