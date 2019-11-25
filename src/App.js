a4ff5fdc40f98f82da12fd5227112ca6c65d79ce
Yuri Lee <leyuri97@gmail.com>
2019-11-25 23:27:39 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   
@@ -32,25 +51,46 @@ const customers = [
 
 class App extends Component {
   render(){
+    const { classes } = this.props;
     return(
-      <div>
-        {
-          // map 함수를 이용함으로써 특정한 배열의 각 원소에 접근하여 어떻게 처리할지 명시할 수 있다. 
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                //map을 이용할 때는 key를 사용해야 한다
-                id={c.id}
-                image={c.name}
-                name={c.name}
-                birthday={c.name}
-                gender={c.gender}
-                job={c.job}
-              />
-              );
-            })
-          }
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+            <TableBody>
+                {/* map 함수를 이용함으로써 특정한 배열의 각 원소에 접근하여 어떻게 처리할지 명시할 수 있다.  */}
+              {customers.map(c => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      //map을 이용할 때는 key를 사용해야 한다
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                    );
+                  })
+              }
+            </TableBody>
+        </Table>
+
+
+
+
+
+    
+
       {/* <Customer
         id={customers[0].id}
         image={customers[0].image}
@@ -75,9 +115,9 @@ class App extends Component {
         gender={customers[2].gender}
         job={customers[2].job}
       /> */}
-      </div>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
